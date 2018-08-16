fb72fb50214400556793f2b93ce6c103d8950403
Fachruzi Ramadhan <mabuak@live.com>
2018-08-16 19:27:35 +0700

    Add timer

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    var time = 210
+    
+    var isTimerRun = true
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func play(_ sender: Any) {
+        
+        if isTimerRun {
+            timer.invalidate()
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(decreaseTimer), userInfo: nil, repeats: true)
+        
+        isTimerRun = true
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func plusTen(_ sender: Any) {
+        time += 10
+        
+        timerLabel.text = String(time)
+    }
+    
+    @IBAction func minusTen(_ sender: Any) {
+        
+        if time > 10 {
+        
+            time -= 10
+            
+            timerLabel.text = String(time)
+            
+        }
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        time = 210
+        
+        timerLabel.text = String(time)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +66,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func decreaseTimer(){
+        
+        if time > 0 {
+            time -= 1
+            
+            timerLabel.text = String(time)
+        } else {
+            timer.invalidate()
+        }
+    }
 }
 
